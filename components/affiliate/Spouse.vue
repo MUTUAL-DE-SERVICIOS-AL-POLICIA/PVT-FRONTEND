f9720d81aa0bad5969352fd22fe3768e557b8311
gladys850 <gpq850@gmail.com>
2022-12-06 07:25:13 -0400

    Adicionando validacion borrada

--- components/affiliate/Spouse.vue
+++ components/affiliate/Spouse.vue
@@ -3,7 +3,7 @@
     <v-form ref="forSpouse">
       <v-row class="text-right">
         <v-col cols="12" class="ma-0 pa-0 pt-0">
-          <template v-if="!this.spouse.id"
+          <template v-if="!this.spouse.id && primaryPermission"
             ><v-btn
               x-small
               dark
@@ -23,7 +23,7 @@
               <span> Cancelar</span>
             </v-btn>
           </template>
-          <template v-if="this.spouse.id">
+          <template v-if="(this.spouse.id && primaryPermission) || (this.spouse.id && secondaryPermission)">
             <v-btn
               x-small
               right
@@ -414,7 +414,7 @@ export default {
           }
         }
       } catch (e) {
-        this.sync_up.editable = false;
+        console.log(e)
         this.$toast.error("Ocurrió un error durante la creación inconsistencia de datos.");
       }
     },
@@ -443,7 +443,7 @@ export default {
           }
         }
       } catch (e) {
-        this.sync_up.editable = false;
+        console.log(e)
         this.$toast.error("Ocurrio un error durante la actualización.");
       }
     },

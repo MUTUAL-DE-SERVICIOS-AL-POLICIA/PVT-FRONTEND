eae853748e244538b7fceb6f76dc9b091dc2d8be
gladys850 <gpq850@gmail.com>
2023-03-21 12:59:43 -0400

    movel notificaciones

---
+++ pages/reports/PageReportNotification.vue
@@ -0,0 +1,13 @@
+<template>
+    <ReportNotification/>
+</template>
+
+<script>
+import ReportNotification from "@/components/reports/ReportNotification.vue";
+export default {
+  name: "MainReportNotication",
+  components: {
+    ReportNotification,
+  },
+};
+</script>

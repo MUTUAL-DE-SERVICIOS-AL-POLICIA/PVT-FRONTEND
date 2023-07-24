bb3e27109ca937b355a41e7280f31d2db44a2cb0
gpatty <gpq850@gmail.com>
2023-07-24 02:36:35 -0400

    Cambio de diseño mediante menu en importacion de planillas

---
+++ pages/affiliate/PageImportationAffiliateAvailability.vue
@@ -0,0 +1,32 @@
+<template>
+    <MainImportation :item_import="item_import" />
+</template>
+
+<script>
+import MainImportation from "@/components/contribution/MainImportation.vue";
+export default {
+  name: "PageImportation",
+  components: {
+    MainImportation,
+  },
+  data: () => ({
+    item_import: {
+        id: 4,
+        name: 'DISPONIBILIDAD',
+        import_type_name:"availability",
+        permissions_create: 'create-import-payroll-senasir',//falta
+        permissions_download: 'download-report-payroll-senasir',//falta
+        route_get_months: '/affiliate/list_months_import_affiliates_availability',
+        route_upload_file: '/affiliate/upload_copy_affiliates_availability', //Step1
+        route_validate_data: '/affiliate/validate_availability', //step2
+        download_error_data_archive: 'affiliate/download_error_data_archive',//Step1
+        rollback_import_affiliates_availability: '/affiliate/rollback_import_affiliates_availability',//falta
+        route_import_progressBar: '/affiliate/import_affiliates_availability_progress_bar',
+        download_data_revision_suggestion: '/affiliate/download_data_revision_suggestion', //paso2
+        //name_download_file: "ReporteMatriculasNoValidas.xls",
+        download_data_revision: '/affiliate/download_data_revision', //paso2
+        //name_report_file: "ReporteDatosSenasir.xls"
+      }
+  })
+};
+</script>
